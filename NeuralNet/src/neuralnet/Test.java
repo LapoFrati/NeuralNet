aefa9390a6afec07b9af63647a76208d7886e4d5
LapoFrati <fratilapo@gmail.com>
2015-03-23 14:02:52 +0000

    Add OptionsReader test

---
+++ NeuralNet/src/neuralnet/Test.java
@@ -0,0 +1,14 @@
+package neuralnet;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		InputReader in = new InputReader();
+		
+		in.readOptions("Options.txt");
+		
+	}
+
+}
