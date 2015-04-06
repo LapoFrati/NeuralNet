dfcb397e2b0a880059e479bce45f4a3225d23979
LapoFrati <fratilapo@gmail.com>
2015-04-06 12:15:02 +0100

    Move InputTest to test folder

--- NeuralNet/test/neuralnet/InputTest.java
+++ NeuralNet/test/neuralnet/InputTest.java
@@ -2,7 +2,7 @@ package neuralnet;
 
 import java.io.IOException;
 
-public class Test {
+public class InputTest {
 
 	public static void main(String[] args) throws IOException {
 		InputReader in = new InputReader();
